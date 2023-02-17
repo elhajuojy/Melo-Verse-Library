a8bd6a3c56892c853e1e96b43e1f58c6c4f4b07b
elhajuojy <souirimehdi311@gmail.com>
2023-02-17 20:27:00 +0100

    add albums

---
+++ database/seeders/AlbumSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Album;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AlbumSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
