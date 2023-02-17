e6dcfbcb4863901312535f81e375f941cf88e117
elhajuojy <souirimehdi311@gmail.com>
2023-02-17 20:02:07 +0100

    add is admin filed

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,6 +22,7 @@ return new class extends Migration
             $table->string('location')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean("is_admin")->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
