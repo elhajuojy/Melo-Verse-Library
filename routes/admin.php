2f17cf198d01ac592189d300a6d42beb8e644601
elhajuojy <souirimehdi311@gmail.com>
2023-03-20 10:58:14 +0000

    add more routes for the songs

--- routes/admin.php
+++ routes/admin.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\AdminController;
 
 
 
-Route::get("/admin",[AdminController::class,"index"])->name("admin")->middleware("admin");
+Route::get("/admin",[AdminController::class,"songs"])->name("admin")->middleware("admin");
 Route::get('/admin/albums',[AdminController::class,"albums"])->name("admin.albums")->middleware("admin");
 Route::get('/admin/artists',[AdminController::class,"artists"])->name("admin.artists")->middleware("admin");
 Route::get('/admin/Genres',[AdminController::class,"Genres"])->name("admin.Genres")->middleware("admin");
@@ -15,6 +15,11 @@ Route::get('/admin/Genres',[AdminController::class,"Genres"])->name("admin.Genre
 
 Route::post("/admin/songs",[AdminController::class,"store"])->name("admin.songs")->middleware("admin");
 Route::get("/admin/songs",[AdminController::class,"songs"])->name("admin.songs")->middleware("admin");
+Route::get("/admin/songs/create",[AdminController::class,"create"])->name("admin.songs.create")->middleware("admin");
+Route::get("/admin/songs/{song}/edit",[AdminController::class,"edit"])->name("admin.songs.edit")->middleware("admin");
+Route::patch("/admin/songs/{song}/archive",[AdminController::class,"archiveSong"])->name("admin.songs.archive")->middleware("admin");
+Route::patch("/admin/songs/{song}/unarchive",[AdminController::class,"unArchiveSong"])->name("admin.songs.unarchive")->middleware("admin");
+
 
 
 
